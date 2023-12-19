c86233ef7698bb30e71cfd273b50f4a8fb97c696
ngortiz <nidigortiz21@gmail.com>
2023-12-19 15:36:11 -0300

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <SmartSeller />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<BrowserRouter>
+		<React.StrictMode>
+			<NavBar />
+		</React.StrictMode>
+	</BrowserRouter>,
 );
