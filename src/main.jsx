431ae12ef129d0937fa7ab1075bebaf518643638
Nidia Ortiz <123881386+ngortiz@users.noreply.github.com>
2023-12-21 10:24:04 -0300

    Fix isues (#3)

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import { BrowserRouter } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<BrowserRouter>
-		<React.StrictMode>
-			<NavBar />
-		</React.StrictMode>
-	</BrowserRouter>,
-);
+  <BrowserRouter>
+    <React.StrictMode>
+      <NavBar />
+    </React.StrictMode>
+  </BrowserRouter>
+)
