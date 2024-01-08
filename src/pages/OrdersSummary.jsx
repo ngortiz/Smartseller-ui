aa01bc18e1aa544b7a525be0bb3503c7547446f9
ngortiz <nidigortiz21@gmail.com>
2024-01-08 10:57:13 -0300

    Remove curly braces

--- src/pages/OrdersSummary.jsx
+++ src/pages/OrdersSummary.jsx
@@ -14,7 +14,6 @@ const OrdersSummary = () => {
       <Row className='order-summary-container'></Row>
       <Row>
         <Col xs={12} sm={6} md={4} lg={3} className='px-2'>
-          {' '}
           <OrderStatus status='Recientes' amount={4} color='#00c0ef' />
         </Col>
         <Col xs={12} sm={6} md={4} lg={3} className='px-2'>
