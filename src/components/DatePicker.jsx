1febfb08a3bd267071bb23577c649ada102c329b
ngortiz <nidigortiz21@gmail.com>
2024-01-08 22:18:36 -0300

    DatePicker

---
+++ src/components/DatePicker.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
+const DatePicker = () => {
+  return (
+    <>
+      <DatePicker />
+    </>
+  )
+}
+export default DatePicker
