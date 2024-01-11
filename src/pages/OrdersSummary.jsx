33733d5ea485954330888a9c55f71d4684d9f64f
ngortiz <nidigortiz21@gmail.com>
2024-01-11 16:43:05 -0300

    Search button added

--- src/pages/OrdersSummary.jsx
+++ src/pages/OrdersSummary.jsx
@@ -16,6 +16,10 @@ const OrdersSummary = () => {
   const handleEndDateChange = date => {
     setEndDate(date)
   }
+  const handleSearch = () => {
+    console.log('Buscar...')
+  }
+
   return (
     <Container fluid>
       <Row className='title-container'>
@@ -46,22 +50,35 @@ const OrdersSummary = () => {
           <OrderStatus status='Atendidos' amount={1} color='#222222' />
         </Col>
       </Row>
-      <Row>
+
+      <Row className='order-summary-container'></Row>
+      <Row className='justify-content-between'>
         <Col>
           <div className='date-picker-container'>
-            <h1 className='date-picker-label'> Inicio Fecha de Creacion: </h1>
-            <CustomDatePicker
-              selectedDate={startDate}
-              handleChange={handleStartDateChange}
-            />
+            <h1 className='date-picker-label'>Inicio Fecha de Creación: </h1>
+            <div className='input-group-addon'>
+              <CustomDatePicker
+                selectedDate={startDate}
+                handleChange={handleStartDateChange}
+              />
+            </div>
           </div>
+        </Col>
+        <Col>
           <div className='date-picker-container'>
-            <h1 className='date-picker-label'> Fin Fecha de Creacion: </h1>
-            <CustomDatePicker
-              selectedDate={endDate}
-              handleChange={handleEndDateChange}
-            />
+            <h1 className='date-picker-label'>Fin Fecha de Creación: </h1>
+            <div className='input-group-addon'>
+              <CustomDatePicker
+                selectedDate={endDate}
+                handleChange={handleEndDateChange}
+              />
+            </div>
           </div>
+        </Col>
+        <Col>
+          <button className='search-button' onClick={handleSearch}>
+            Buscar
+          </button>
         </Col>
       </Row>
     </Container>
