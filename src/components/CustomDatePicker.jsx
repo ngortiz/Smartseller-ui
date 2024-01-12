25da20b6405df1d5e65bc39b4163140ca92ed705
Nidia Ortiz <123881386+ngortiz@users.noreply.github.com>
2024-01-12 09:22:49 -0300

    Date picker (#10)
    * OrderSummaryStatus
    
    * OrderSummary
    
    * OrderSummaryStatus
    
    * OrderSummaryStatus
    
    * OrderSummarySatus
    
    * OrderSummaryStatus
    
    * OrderSummaryStatus
    
    * Improved code
    
    * Remove curly braces
    
    * DatePicker
    
    * DatePicker
    
    * DatePicker
    
    * A Row and 2 column are added
    
    * remove icon
    
    * Search button added
    
    * Style change
    
    * Style change
    
    * remove row

---
+++ src/components/CustomDatePicker.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import './CustomDatePicker.css'
+import { BiCalendar } from 'react-icons/bi'
+
+const CustomDatePicker = ({ selectedDate, handleChange }) => {
+  return (
+    <div className='custom-date-picker'>
+      <BiCalendar className='bi bi-calendar-icon' />
+      <DatePicker
+        selected={selectedDate}
+        onChange={handleChange}
+        dateFormat='dd/MM/yyyy'
+        className='date-picker-input'
+      />
+    </div>
+  )
+}
+CustomDatePicker.propTypes = {
+  selectedDate: PropTypes.instanceOf(Date),
+  handleChange: PropTypes.func.isRequired
+}
+export default CustomDatePicker
--- src/pages/OrdersSummary.jsx
+++ src/pages/OrdersSummary.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import OrderStatus from '../components/OrderStatus'
 import './OrdersSummary.css'
+import CustomDatePicker from '../components/CustomDatePicker'
 
 const OrdersSummary = () => {
+  const defaultDate = new Date()
+  const [startDate, setStartDate] = useState(defaultDate)
+  const [endDate, setEndDate] = useState(defaultDate)
+  const handleStartDateChange = date => {
+    setStartDate(date)
+  }
+  const handleEndDateChange = date => {
+    setEndDate(date)
+  }
+  const handleSearch = () => {
+    console.log('Buscar...')
+  }
+
   return (
     <Container fluid>
       <Row className='title-container'>
@@ -11,7 +25,6 @@ const OrdersSummary = () => {
           <h2 className='title'>Resumen de Pedidos</h2>
         </Col>
       </Row>
-      <Row className='order-summary-container'></Row>
       <Row>
         <Col xs={12} sm={6} md={4} lg={3} className='px-2'>
           <OrderStatus status='Recientes' amount={4} color='#00c0ef' />
@@ -33,6 +46,37 @@ const OrdersSummary = () => {
         </Col>
         <Col xs={12} sm={6} md={4} lg={3} className='px-2'>
           <OrderStatus status='Atendidos' amount={1} color='#222222' />
+        </Col>
+      </Row>
+
+      <Row className='order-summary-container'></Row>
+      <Row className='justify-content-between'>
+        <Col>
+          <div className='date-picker-container'>
+            <h1 className='date-picker-label'>Inicio Fecha de Creación: </h1>
+            <div className='input-group-addon'>
+              <CustomDatePicker
+                selectedDate={startDate}
+                handleChange={handleStartDateChange}
+              />
+            </div>
+          </div>
+        </Col>
+        <Col>
+          <div className='date-picker-container'>
+            <h1 className='date-picker-label'>Fin Fecha de Creación: </h1>
+            <div className='input-group-addon'>
+              <CustomDatePicker
+                selectedDate={endDate}
+                handleChange={handleEndDateChange}
+              />
+            </div>
+          </div>
+        </Col>
+        <Col>
+          <button className='search-button' onClick={handleSearch}>
+            Buscar
+          </button>
         </Col>
       </Row>
     </Container>
