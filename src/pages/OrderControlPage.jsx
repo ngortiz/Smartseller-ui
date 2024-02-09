ae33d03d7d511e6a92c6a2f37637ad2f4fc1c203
ngortiz <nidigortiz21@gmail.com>
2024-02-08 21:48:24 -0300

    Modifications OrderControl

--- src/pages/OrderControlPage.jsx
+++ src/pages/OrderControlPage.jsx
@@ -1,38 +1,80 @@
 import React from 'react'
+import { Card, Col, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import { Table } from 'react-bootstrap' // Asegúrate de importar Table desde react-bootstrap si lo estás usando
+import './OrderControlPage.css'
 
 const OrderControlPage = () => {
   const orders = [
-    { id: 1, number: 'ORD-001', client: 'Cliente 1', state: 'Atendido' },
-    { id: 2, number: 'ORD-002', client: 'Cliente 2', state: 'Preparando' }
+    {
+      id: 1,
+      number: 'ORD-001',
+      client: 'Cliente 1',
+      state: 'Atendido',
+      product_quantity: '1',
+      payment_method: 'Deposito',
+      created_date: '08-02-2024 10:10'
+    },
+    {
+      id: 2,
+      number: 'ORD-002',
+      client: 'Camila Vera',
+      state: 'No Atendidos',
+      product_quantity: '6',
+      payment_method: 'Sucursal',
+      created_date: '08-02-2024 10:10'
+    }
+  ]
+  const orderColumns = [
+    { title: 'No Atendidos', filterState: 'No Atendidos' },
+    { title: 'Preparando', filterState: 'Preparando' },
+    { title: 'Preparados', filterState: 'Preparados' },
+    { title: 'Enviando', filterState: 'Enviando' },
+    { title: 'Sucursal', filterState: 'Sucursal' },
+    { title: 'Atendidos', filterState: 'Atendidos' }
   ]
 
   return (
     <div>
-      <h1>Control de Pedidos</h1>
-      <Table striped bordered hover className='Table'>
-        <thead>
-          <tr>
-            <th>Item</th>
-            <th>Número</th>
-            <th>Cliente</th>
-            <th>Estado Pedido</th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((order, index) => (
-            <tr key={order.id}>
-              <td>{index + 1}</td>
-              <td>
-                <Link to={`/orders/${order.id}`}>{order.number}</Link>
-              </td>
-              <td>{order.client}</td>
-              <td>{order.state}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      <header>Control de Pedidos</header>
+      <Row>
+        {orderColumns.map((column, index) => (
+          <Col key={index} md={1} className='mb-4'>
+            <div className='col-title-container'>
+              <h2 className='col-title'>{column.title}</h2>
+            </div>
+            {orders.map(order => {
+              if (order.state === column.filterState) {
+                return (
+                  <Card key={order.id} className='mb-3'>
+                    <Card.Body className='card-body'>
+                      <Card.Title className='title-card'>
+                        Detalle de compra
+                      </Card.Title>
+                      <Card.Text className='Text-card'>
+                        <span className='client-name'>Cliente:</span>
+                        <br /> {order.client}
+                        <br />
+                        <span className='quantity'>Cant. Pedido:</span>
+                        <br /> {order.product_quantity}
+                        <br />
+                        <span className='order-number'>Pedido Numero:</span>
+                        <br /> {order.number}
+                      </Card.Text>
+                      <Link
+                        to={`/orders/${order.id}`}
+                        className='btn btn-primary'
+                      >
+                        Ver Detalles
+                      </Link>
+                    </Card.Body>
+                  </Card>
+                )
+              }
+              return null
+            })}
+          </Col>
+        ))}
+      </Row>
     </div>
   )
 }
