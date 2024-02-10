b6ba39c2a2b027afc4c0ce0b4f838c743cd6d5aa
ngortiz <nidigortiz21@gmail.com>
2024-02-09 22:25:04 -0300

    New styles for OrderControlPage

--- src/pages/OrderControlPage.jsx
+++ src/pages/OrderControlPage.jsx
@@ -6,24 +6,16 @@ import './OrderControlPage.css'
 const OrderControlPage = () => {
   const orders = [
     {
-      id: 1,
-      number: 'ORD-001',
-      client: 'Cliente 1',
-      state: 'Atendido',
-      product_quantity: '1',
-      payment_method: 'Deposito',
-      created_date: '08-02-2024 10:10'
-    },
-    {
       id: 2,
       number: 'ORD-002',
       client: 'Camila Vera',
-      state: 'No Atendidos',
+      state: 'Preparando',
       product_quantity: '6',
       payment_method: 'Sucursal',
       created_date: '08-02-2024 10:10'
     }
   ]
+
   const orderColumns = [
     { title: 'No Atendidos', filterState: 'No Atendidos' },
     { title: 'Preparando', filterState: 'Preparando' },
@@ -33,45 +25,75 @@ const OrderControlPage = () => {
     { title: 'Atendidos', filterState: 'Atendidos' }
   ]
 
+  const getClassForState = state => {
+    switch (state) {
+      case 'No Atendidos':
+        return 'estado-no-atendido'
+      case 'Preparando':
+        return 'estado-preparando'
+      case 'Preparados':
+        return 'estado-preparado'
+      case 'Enviando':
+        return 'estado-enviando'
+      case 'Sucursal':
+        return 'estado-sucursal'
+      case 'Atendidos':
+        return 'estado-atendido'
+      default:
+        return ''
+    }
+  }
+
   return (
-    <div>
-      <header>Control de Pedidos</header>
+    <div className='order-control-container'>
+      <header className='order-control-header'>Control de Pedidos</header>
       <Row>
         {orderColumns.map((column, index) => (
-          <Col key={index} md={1} className='mb-4'>
-            <div className='col-title-container'>
-              <h2 className='col-title'>{column.title}</h2>
-            </div>
-            {orders.map(order => {
-              if (order.state === column.filterState) {
-                return (
-                  <Card key={order.id} className='mb-3'>
-                    <Card.Body className='card-body'>
-                      <Card.Title className='title-card'>
-                        Detalle de compra
-                      </Card.Title>
-                      <Card.Text className='Text-card'>
-                        <span className='client-name'>Cliente:</span>
-                        <br /> {order.client}
-                        <br />
-                        <span className='quantity'>Cant. Pedido:</span>
-                        <br /> {order.product_quantity}
-                        <br />
-                        <span className='order-number'>Pedido Numero:</span>
-                        <br /> {order.number}
-                      </Card.Text>
-                      <Link
-                        to={`/orders/${order.id}`}
-                        className='btn btn-primary'
-                      >
-                        Ver Detalles
-                      </Link>
-                    </Card.Body>
-                  </Card>
-                )
-              }
-              return null
-            })}
+          <Col key={index} md={4} className='order-column-title '>
+            <Card
+              className={`order-card ${getClassForState(column.filterState)}`}
+            >
+              <Card.Header className='order-column-title '>
+                {column.title}
+              </Card.Header>
+              <Card.Body>
+                {orders.map(order => {
+                  if (order.state === column.filterState) {
+                    return (
+                      <div key={order.id} className='mb-3'>
+                        <Card.Title className='order-card-title'>
+                          Detalle de compra
+                        </Card.Title>
+                        <Card.Text className='order-card-text'>
+                          <span className='order-control-span'>Cliente</span>
+                          <span className='separator'>:</span>
+                          {order.client}
+                          <br />
+                          <span className='order-control-span'>
+                            Cant. Pedido
+                          </span>
+                          <span className='separator'>:</span>
+                          {order.product_quantity}
+                          <br />
+                          <span className='order-control-span'>
+                            Pedido Numero
+                          </span>
+                          <span className='separator'>:</span>
+                          {order.number}
+                        </Card.Text>
+                        <Link
+                          to={`/orders/${order.id}`}
+                          className='order-card-button'
+                        >
+                          Ver Detalles
+                        </Link>
+                      </div>
+                    )
+                  }
+                  return null
+                })}
+              </Card.Body>
+            </Card>
           </Col>
         ))}
       </Row>
