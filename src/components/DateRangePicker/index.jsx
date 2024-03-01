f5b36106c3f39baa88c537c793ba38660ac24f46
ngortiz <nidigortiz21@gmail.com>
2024-03-01 00:01:49 -0300

    Modifications GeneralArrangemnt

--- src/components/DateRangePicker/index.jsx
+++ src/components/DateRangePicker/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Row, Col, Button } from 'react-bootstrap'
 import CustomDatePicker from '../CustomDatePicker'
+import './style.css'
 const DateRangePicker = ({
   startDate,
   endDate,
@@ -11,21 +12,18 @@ const DateRangePicker = ({
   return (
     <div className='date-range-picker-container'>
       <Row>
-        <Col className='date-picker-container'>
+        <Col className='date-picker-container '>
           <h1 className='custom-label'>Desde: </h1>
           <CustomDatePicker
             selectedDate={startDate}
             handleChange={handleStartDateChange}
           />
-        </Col>
-        <Col className='date-picker-container'>
+
           <h1 className='custom-label'>Hasta: </h1>
           <CustomDatePicker
             selectedDate={endDate}
             handleChange={handleEndDateChange}
           />
-        </Col>
-        <Col className='search-button-container'>
           <Button
             variant='primary'
             className='date-button'
