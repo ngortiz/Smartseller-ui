306b50cf1224a0fba8035b53a63608774625cf7b
ngortiz <nidigortiz21@gmail.com>
2024-03-08 14:29:26 -0300

    Apollo client applies

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import { Container, Row, Col } from 'react-bootstrap'
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  ApolloProvider,
+  gql
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 
 import LeftNavigationBar from './components/LeftNavigationBar'
 import OrdersSummary from './pages/OrdersSummary'
@@ -12,29 +20,49 @@ import OrderBoardPage from './pages/OrderBoardPage'
 import CreditCardPayments from './pages/CreditCardPayments'
 import BankPaymentsPage from './pages/BankPaymentsPage'
 
+const httpLink = createHttpLink({
+  uri: import.meta.env.VITE_API_URL
+})
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: import.meta.env.VITE_ACCESS_TOKEN
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
+
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
-      <Container fluid>
-        <Row>
-          <Col xs={3}>
-            <LeftNavigationBar />
-          </Col>
-          <Col xs={9}>
-            <Routes>
-              <Route path='/orders/summary' element={<OrdersSummary />} />
+      <ApolloProvider client={client}>
+        <Container fluid>
+          <Row>
+            <Col xs={3}>
+              <LeftNavigationBar />
+            </Col>
+            <Col xs={9}>
+              <Routes>
+                <Route path='/orders/summary' element={<OrdersSummary />} />
 
-              <Route path='/order/board' element={<OrderBoardPage />} />
-              <Route
-                path='/credit-card-payments'
-                element={<CreditCardPayments />}
-              />
-              <Route path='/bank-payments' element={<BankPaymentsPage />} />
-              <Route path='/orders/:id' element={<OrderInformationPage />} />
-            </Routes>
-          </Col>
-        </Row>
-      </Container>
+                <Route path='/order/board' element={<OrderBoardPage />} />
+                <Route
+                  path='/credit-card-payments'
+                  element={<CreditCardPayments />}
+                />
+                <Route path='/bank-payments' element={<BankPaymentsPage />} />
+                <Route path='/orders/:id' element={<OrderInformationPage />} />
+              </Routes>
+            </Col>
+          </Row>
+        </Container>
+      </ApolloProvider>
     </React.StrictMode>
   </BrowserRouter>
 )
