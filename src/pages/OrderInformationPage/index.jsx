a754f2f50d411e3aea21cfcf8fae13b83260dd60
ngortiz <nidigortiz21@gmail.com>
2024-04-11 08:27:58 -0400

    deleted

--- src/pages/OrderInformationPage/index.jsx
+++ src/pages/OrderInformationPage/index.jsx
@@ -65,7 +65,6 @@ const OrderInformationPage = () => {
 
   useEffect(() => {
     if (data && data.getOrder) {
-      console.log(data.getOrder)
       setOrder(data.getOrder);
     }
   }, [loading]);
