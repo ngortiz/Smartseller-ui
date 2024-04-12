a4fed1ca476c83aecc951135ce42459b228e0873
Nidia Ortiz <123881386+ngortiz@users.noreply.github.com>
2024-04-12 10:12:27 -0400

    Modifications (#34)
    * Modifications
    
    * Modifications
    
    * MODIFICATIONS

--- src/components/OrderData/index.jsx
+++ src/components/OrderData/index.jsx
@@ -1,36 +1,38 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Col } from 'react-bootstrap'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Col } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 export const OrderData = ({ number, voucher, state, date }) => {
-  return (
-    <div className='column-content'>
-      <Col className='order-info-col'>
-        <h2 className='title'>Pedido</h2>
+	const { t } = useTranslation();
 
-        <p>
-          <strong>Nro.Pedido:</strong> <label>{number}</label>
-        </p>
-        <p>
-          <strong>Nro.Comprobante(Pagopar):</strong>
-          <label>{voucher}</label>
-        </p>
-        <p>
-          <strong>Estado del Pedido:</strong> <label>{state}</label>
-        </p>
-        <p>
-          <strong>Fecha de Pedido:</strong> <label>{date}</label>
-        </p>
-      </Col>
-    </div>
-  )
-}
+	return (
+		<div className='column-content'>
+			<Col className='order-info-col'>
+				<h2 className='title'>{t('orderData.title')}</h2>
+				<p>
+					<strong>{t('orderData.number')}:</strong> <label>{number}</label>
+				</p>
+				<p>
+					<strong>{t('orderData.voucher')}:</strong> <label>{voucher}</label>
+				</p>
+				<p>
+					<strong>{t('orderData.state')}:</strong>
+					{state ? t(`orderStatus.${state}`) : ''}
+				</p>
+				<p>
+					<strong>{t('orderData.date')}:</strong> <label>{date}</label>
+				</p>
+			</Col>
+		</div>
+	);
+};
 
 OrderData.propTypes = {
-  number: PropTypes.string.isRequired,
-  voucher: PropTypes.string.isRequired,
-  state: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired
-}
+	number: PropTypes.string.isRequired,
+	voucher: PropTypes.string.isRequired,
+	state: PropTypes.string.isRequired,
+	date: PropTypes.string.isRequired,
+};
 
-export default OrderData
+export default OrderData;
--- src/components/OrderPayment/index.jsx
+++ src/components/OrderPayment/index.jsx
@@ -1,33 +1,38 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import './style.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.css';
+import { useTranslation } from 'react-i18next';
 
 export const OrderPayment = ({ paymentState, total, totalPaid, totalDebt }) => {
-  return (
-    <div className='column-content'>
-      <h2 className='title'>Pago</h2>
-      <p>
-        <strong>Estado de Pago:</strong>
-        <label>{paymentState}</label>
-      </p>
-      <p>
-        <strong>Total:</strong> <label>{total}</label>
-      </p>
-      <p>
-        <strong>Total Pagado:</strong> <label>{totalPaid}</label>
-      </p>
-      <p>
-        <strong>Total Deuda:</strong> <label>{totalDebt}</label>
-      </p>
-    </div>
-  )
-}
+	const { t } = useTranslation();
+
+	return (
+		<div className='column-content'>
+			<h2 className='title'>{t('orderPayment.title')}</h2>
+			<p>
+				<strong>{t('orderPayment.paymentState')}:</strong>
+				{t(`paymentStatus.${paymentState.toLowerCase()}`)}
+			</p>
+			<p>
+				<strong>{t('orderPayment.total')}:</strong> <label>{total}</label>
+			</p>
+			<p>
+				<strong>{t('orderPayment.totalPaid')}:</strong>{' '}
+				<label>{totalPaid}</label>
+			</p>
+			<p>
+				<strong>{t('orderPayment.totalDebt')}:</strong>{' '}
+				<label>{totalDebt}</label>
+			</p>
+		</div>
+	);
+};
 
 OrderPayment.propTypes = {
-  paymentState: PropTypes.string.isRequired,
-  total: PropTypes.string.isRequired,
-  totalPaid: PropTypes.string.isRequired,
-  totalDebt: PropTypes.string.isRequired
-}
+	paymentState: PropTypes.string.isRequired,
+	total: PropTypes.string.isRequired,
+	totalPaid: PropTypes.string.isRequired,
+	totalDebt: PropTypes.string.isRequired,
+};
 
-export default OrderPayment
+export default OrderPayment;
