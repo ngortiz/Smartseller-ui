721c3fb8b989e2120f2197693a6094736fcd09ad
ngortiz <nidigortiz21@gmail.com>
2024-04-15 16:31:38 -0400

    add query  OderBoard

--- src/components/OrderCard/index.jsx
+++ src/components/OrderCard/index.jsx
@@ -1,52 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import { Card } from 'react-bootstrap'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Card } from 'react-bootstrap';
 
-import './style.css'
+import './style.css';
 
 const OrderCard = ({ order, getClassForState, column }) => {
-  return (
-    <div>
-      <Card
-        key={order.id}
-        className={`order-card ${getClassForState(column.filterState)}`}
-      >
-        <Card.Body className='order-card-content'>
-          <Card.Text className='order-card-text'>
-            <span className='texto-spam'>
-              <i className='bi bi-bag-fill'></i>
-              <Link to={`/orders/${order.id}`}>{order.number}</Link>
-            </span>
-            <span className='texto-spam'>
-              <i className='bi bi-person-square'></i>
-              {order.client}
-            </span>
+	return (
+		<div>
+			<Card
+				key={order.id}
+				className={`order-card ${getClassForState(column.filterState)}`}
+			>
+				<Card.Body className='order-card-content'>
+					<Card.Text className='order-card-text'>
+						<span className='texto-spam'>
+							<i className='bi bi-bag-fill'></i>
+							<Link to={`/orders/${order.id}`}>{order.number}</Link>
+						</span>
+						<span className='texto-spam'>
+							<i className='bi bi-person-square'></i>
+							{order.username}
+						</span>
 
-            <span className='texto-spam'>
-              <i className='bi bi-calendar-check'></i>
-              {order.endCreation}
-            </span>
+						<span className='texto-spam'>
+							<i className='bi bi-calendar-check'></i>
+							{order.createdAt}
+						</span>
 
-            <span className='texto-spam'>
-              <i className='bi bi-credit-card'></i>
-              {order.paymentMethod}
-            </span>
-            <span className='texto-spam'>
-              <i className='bi bi-credit-card'></i>
-              {order.total}
-            </span>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </div>
-  )
-}
+						<span className='texto-spam'>
+							<i className='bi bi-credit-card'></i>
+							{order.buyMethod}
+						</span>
+						<span className='texto-spam'>
+							<i className='bi bi-credit-card'></i>
+							{order.total}
+						</span>
+					</Card.Text>
+				</Card.Body>
+			</Card>
+		</div>
+	);
+};
 
 OrderCard.propTypes = {
-  order: PropTypes.object.isRequired,
-  getClassForState: PropTypes.func.isRequired,
-  column: PropTypes.object.isRequired
-}
+	order: PropTypes.object.isRequired,
+	getClassForState: PropTypes.func.isRequired,
+	column: PropTypes.object.isRequired,
+};
 
-export default OrderCard
+export default OrderCard;
--- src/pages/OrderBoardPage/index.jsx
+++ src/pages/OrderBoardPage/index.jsx
@@ -1,158 +1,141 @@
-import React, { useState } from 'react'
-import { Col, Row } from 'react-bootstrap'
-import DateRangePicker from '../../components/DateRangePicker'
-import './style.css'
+import React, { useState, useEffect } from 'react';
+import { Col, Row } from 'react-bootstrap';
+import DateRangePicker from '../../components/DateRangePicker';
+import './style.css';
+import { useLazyQuery, gql } from '@apollo/client';
 
-import OrderCard from '../../components/OrderCard'
-import { DndContext } from '@dnd-kit/core'
-import Droppable from '../../components/Droppable'
-import OrderDraggable from '../../components/OrderDraggable'
+import OrderCard from '../../components/OrderCard';
+import { DndContext } from '@dnd-kit/core';
+import Droppable from '../../components/Droppable';
+import OrderDraggable from '../../components/OrderDraggable';
+
+const GET_ORDERS_QUERY = gql`
+	query GetOrdersQuery($startDate: AWSDateTime!, $endDate: AWSDateTime!) {
+		getOrders(startDate: $startDate, endDate: $endDate) {
+			buyMethod
+			number
+			username
+			id
+			orderState
+			paymentState
+			updatedAt
+			createdAt
+			total
+		}
+	}
+`;
 
 const OrderBoardPage = () => {
-  const [orders, setOrders] = useState([
-    {
-      id: 1,
-      number: 'ORD-002',
-      client: 'Lisa Vera',
-      state: 'issued',
-      productQuantity: '6',
-      paymentMethod: 'Sucursal',
-      createdDate: '08-02-2024 10:10',
-      total: 'US$ 10.5',
-      endCreation: '08-02-2024'
-    },
-    {
-      id: 2,
-      number: 'ORD-003',
-      client: 'Juan Pérez',
-      state: 'preparing',
-      productQuantity: '3',
-      paymentMethod: 'Deposito',
-      createdDate: '08-02-2024 11:20',
-      total: 'US$ 15.25',
-      endCreation: '08-02-2024'
-    },
-    {
-      id: 3,
-      number: 'ORD-004',
-      client: 'Paula V',
-      state: 'delivering',
-      productQuantity: '3',
-      paymentMethod: 'TC',
-      createdDate: '08-02-2024 11:20',
-      total: 'US$ 8.25',
-      endCreation: '09-02-2024'
-    },
-    {
-      id: 4,
-      number: 'ORD-005',
-      client: 'Enrique v',
-      state: 'dispatched',
-      productQuantity: '3',
-      paymentMethod: 'T.C',
-      createdDate: '10-02-2024 11:20',
-      total: 'US$ 10.25',
-      endCreation: '10-02-2024'
-    }
-  ])
+	const [orders, setOrders] = useState([]);
 
-  const defaultDate = new Date()
-  const [startDate, setStartDate] = useState(defaultDate)
-  const [endDate, setEndDate] = useState(defaultDate)
+	const [getOrders, { loading, error, data }] = useLazyQuery(GET_ORDERS_QUERY);
 
-  const handleStartDateChange = date => {
-    setStartDate(date)
-  }
+	const defaultDate = new Date();
+	const [startDate, setStartDate] = useState(defaultDate);
+	const [endDate, setEndDate] = useState(defaultDate);
 
-  const handleEndDateChange = date => {
-    setEndDate(date)
-  }
-  const handleSearch = () => {}
+	const handleStartDateChange = date => {
+		setStartDate(date);
+	};
 
-  const orderColumns = [
-    { title: 'No Atendidos', filterState: 'issued' },
-    { title: 'Preparando', filterState: 'preparing' },
-    { title: 'Preparados', filterState: 'prepared' },
-    { title: 'Enviando', filterState: 'delivering' },
-    { title: 'Sucursal', filterState: 'ready_to_pickup' },
-    { title: 'Atendidos', filterState: 'dispatched' }
-  ]
+	const handleEndDateChange = date => {
+		setEndDate(date);
+	};
+	const handleSearch = () => {
+		getOrders({ variables: { startDate, endDate } });
+	};
 
-  const getClassForState = state => {
-    switch (state) {
-      case 'issued':
-        return 'state-issued'
-      case 'preparing':
-        return 'state-preparing'
-      case 'prepared':
-        return 'state-prepared'
-      case 'delivering':
-        return 'state-delivering'
-      case 'ready_to_pickup':
-        return 'state-ready_to_pickup'
-      case 'dispatched':
-        return 'state-dispatched'
-      default:
-        return ''
-    }
-  }
+	useEffect(() => {
+		if (data && data.getOrders) {
+			console.log(data);
+			setOrders(data.getOrders);
+		}
+	}, [loading]);
 
-  const updateOrderState = (orderId, newState) => {
-    const updatedOrders = orders.map(order =>
-      order.id === orderId ? { ...order, state: newState } : order
-    )
-    setOrders(updatedOrders)
-  }
+	const orderColumns = [
+		{ title: 'No Atendidos', filterState: 'issued' },
+		{ title: 'Preparando', filterState: 'preparing' },
+		{ title: 'Preparados', filterState: 'prepared' },
+		{ title: 'Enviando', filterState: 'delivering' },
+		{ title: 'Sucursal', filterState: 'ready_to_pickup' },
+		{ title: 'Atendidos', filterState: 'dispatched' },
+	];
 
-  const handleDragEnd = ({ active, over }) => {
-    if (over) {
-      const orderId = parseInt(active.id)
-      const newState = over.id
-      updateOrderState(orderId, newState)
-    }
-  }
+	const getClassForState = state => {
+		switch (state) {
+			case 'issued':
+				return 'state-issued';
+			case 'preparing':
+				return 'state-preparing';
+			case 'prepared':
+				return 'state-prepared';
+			case 'delivering':
+				return 'state-delivering';
+			case 'ready_to_pickup':
+				return 'state-ready_to_pickup';
+			case 'dispatched':
+				return 'state-dispatched';
+			default:
+				return '';
+		}
+	};
 
-  return (
-    <DndContext onDragEnd={handleDragEnd}>
-      <div className='order-control-container'>
-        <header className='order-control-header'>Control de Pedidos</header>
-        <Row className='row-cols'>
-          <Col className='order-date-col'>
-            <DateRangePicker
-              startDate={startDate}
-              endDate={endDate}
-              handleStartDateChange={handleStartDateChange}
-              handleEndDateChange={handleEndDateChange}
-              handleSearch={handleSearch}
-            />
-          </Col>
-        </Row>
-        <Row className='row-cols'>
-          {orderColumns.map((column, index) => (
-            <Col key={index} md={2} className='column-card'>
-              <div className='column-with-card'>
-                <h4 className='order-column-text'>{column.title}</h4>
-                <Droppable id={column.filterState}>
-                  {orders.map(
-                    order =>
-                      order.state === column.filterState && (
-                        <OrderDraggable key={order.id} id={order.id}>
-                          <OrderCard
-                            order={order}
-                            getClassForState={getClassForState}
-                            column={column}
-                          />
-                        </OrderDraggable>
-                      )
-                  )}
-                </Droppable>
-              </div>
-            </Col>
-          ))}
-        </Row>
-      </div>
-    </DndContext>
-  )
-}
+	const updateOrderState = (orderId, newState) => {
+		const updatedOrders = orders.map(order =>
+			order.id === orderId ? { ...order, state: newState } : order,
+		);
+		setOrders(updatedOrders);
+	};
 
-export default OrderBoardPage
+	const handleDragEnd = ({ active, over }) => {
+		if (over) {
+			const orderId = parseInt(active.id);
+			const newState = over.id;
+			updateOrderState(orderId, newState);
+		}
+	};
+
+	return (
+		<DndContext onDragEnd={handleDragEnd}>
+			<div className='order-control-container'>
+				<header className='order-control-header'>Control de Pedidos</header>
+				<Row className='row-cols'>
+					<Col className='order-date-col'>
+						<DateRangePicker
+							startDate={startDate}
+							endDate={endDate}
+							handleStartDateChange={handleStartDateChange}
+							handleEndDateChange={handleEndDateChange}
+							handleSearch={handleSearch}
+						/>
+					</Col>
+				</Row>
+				<Row className='row-cols'>
+					{orderColumns.map((column, index) => (
+						<Col key={index} md={2} className='column-card'>
+							<div className='column-with-card'>
+								<h4 className='order-column-text'>{column.title}</h4>
+								<Droppable id={column.filterState}>
+									{orders.map(
+										order =>
+											order.orderState === column.filterState && (
+												<OrderDraggable key={order.id} id={order.id}>
+													<OrderCard
+														order={order}
+														getClassForState={getClassForState}
+														column={column}
+													/>
+												</OrderDraggable>
+											),
+									)}
+								</Droppable>
+							</div>
+						</Col>
+					))}
+				</Row>
+			</div>
+		</DndContext>
+	);
+};
+
+export default OrderBoardPage;
