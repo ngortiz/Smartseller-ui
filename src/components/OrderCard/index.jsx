4a70e88db0e6b311360316df6b8111c3631afb8a
Nidia Ortiz <123881386+ngortiz@users.noreply.github.com>
2024-04-18 15:05:15 -0400

    add query  OderBoard (#36)
    * add query  OderBoard
    
    * modifications
    
    * modifications
    
    * modifications
    
    * center spinner
    
    * add firsLoading
    
    ---------
    
    Co-authored-by: wildodecisiv <wmonges@decisiv.com>

--- src/components/OrderCard/index.jsx
+++ src/components/OrderCard/index.jsx
@@ -1,52 +1,57 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import { Card } from 'react-bootstrap'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Card } from 'react-bootstrap';
+import moment from 'moment';
+import { useTranslation } from 'react-i18next';
 
-import './style.css'
+import './style.css';
 
 const OrderCard = ({ order, getClassForState, column }) => {
-  return (
-    <div>
-      <Card
-        key={order.id}
-        className={`order-card ${getClassForState(column.filterState)}`}
-      >
-        <Card.Body className='order-card-content'>
-          <Card.Text className='order-card-text'>
-            <span className='texto-spam'>
-              <i className='bi bi-bag-fill'></i>
-              <Link to={`/orders/${order.id}`}>{order.number}</Link>
-            </span>
-            <span className='texto-spam'>
-              <i className='bi bi-person-square'></i>
-              {order.client}
-            </span>
+	const formattedDate = moment(order.createdAt).format('DD/MM/YYYY HH:mm');
+	const { t } = useTranslation();
 
-            <span className='texto-spam'>
-              <i className='bi bi-calendar-check'></i>
-              {order.endCreation}
-            </span>
+	return (
+		<div>
+			<Card
+				key={order.id}
+				className={`order-card ${getClassForState(column.filterState)}`}
+			>
+				<Card.Body className='order-card-content'>
+					<Card.Text className='order-card-text'>
+						<span className='texto-spam'>
+							<i className='bi bi-bag-fill'></i>
+							<Link to={`/orders/${order.id}`}>{order.number}</Link>
+						</span>
+						<span className='texto-spam'>
+							<i className='bi bi-person-square'></i>
+							{order.username}
+						</span>
 
-            <span className='texto-spam'>
-              <i className='bi bi-credit-card'></i>
-              {order.paymentMethod}
-            </span>
-            <span className='texto-spam'>
-              <i className='bi bi-credit-card'></i>
-              {order.total}
-            </span>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </div>
-  )
-}
+						<span className='texto-spam'>
+							<i className='bi bi-calendar-check'></i>
+							{formattedDate}
+						</span>
+
+						<span className='texto-spam'>
+							<i className='bi bi-credit-card'></i>
+							{t(`buyMethodsShort.${order.buyMethod}`)}
+						</span>
+						<span className='texto-spam'>
+							<i className='bi bi-credit-card'></i>
+							US$ {order.total}
+						</span>
+					</Card.Text>
+				</Card.Body>
+			</Card>
+		</div>
+	);
+};
 
 OrderCard.propTypes = {
-  order: PropTypes.object.isRequired,
-  getClassForState: PropTypes.func.isRequired,
-  column: PropTypes.object.isRequired
-}
+	order: PropTypes.object.isRequired,
+	getClassForState: PropTypes.func.isRequired,
+	column: PropTypes.object.isRequired,
+};
 
-export default OrderCard
+export default OrderCard;
