738ee70967d1b26d37e2968d799100fda4ab0486
ngortiz <nidigortiz21@gmail.com>
2024-05-10 16:04:41 -0400

    Some changes are made to the page and the styles are modified

--- src/pages/CategoriesPage/index.jsx
+++ src/pages/CategoriesPage/index.jsx
@@ -1,11 +1,43 @@
 import React, { useState } from 'react';
-import './style.css';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import './style.css';
 
+const DATA = [
+	{
+		id: 1,
+		categoryName: 'Accesorios',
+		subCategories: [
+			{ id: 100, subCategoryName: 'Bolsos, Carteras o Mochila' },
+			{ id: 101, subCategoryName: 'Utiles Escolares' },
+		],
+	},
+	{
+		id: 2,
+		categoryName: 'Bazar',
+		subCategories: [
+			{ id: 102, subCategoryName: 'Plasticos' },
+			{ id: 103, subCategoryName: 'Ollas y Sartenes' },
+		],
+	},
+];
 const CategoriesPage = () => {
 	const [categoria, setCategoria] = useState('');
 	const [subcategoria, setSubcategoria] = useState('');
 	const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('');
+	const [expandedCategories, setExpandedCategories] = useState([]);
+	const [categories, setCategories] = useState(DATA);
+
+	const toggleCategory = category => {
+		if (expandedCategories.includes(category)) {
+			setExpandedCategories(expandedCategories.filter(cat => cat !== category));
+		} else {
+			setExpandedCategories([...expandedCategories, category]);
+		}
+	};
+
+	const isCategoryExpanded = category => {
+		return expandedCategories.includes(category);
+	};
 
 	const handleGuardarCategoria = () => {
 		console.log('Categoría guardada:', categoria);
@@ -14,7 +46,6 @@ const CategoriesPage = () => {
 	const handleAgregarSubcategoria = () => {
 		console.log('Subcategoría agregada:', subcategoria);
 	};
-
 	return (
 		<Container className='categories-page-container'>
 			<Row>
@@ -58,7 +89,7 @@ const CategoriesPage = () => {
 			<Row>
 				<Col>
 					<div className='add-subcategory-section'>
-						<h3>Crear Subcategoría</h3>
+						<h3>Crear Subcategoria</h3>
 						<Form>
 							<Form.Group>
 								<Form.Label>Categoría:*</Form.Label>
@@ -69,8 +100,9 @@ const CategoriesPage = () => {
 									onChange={e => setCategoriaSeleccionada(e.target.value)}
 								>
 									<option value=''>Seleccione categoria</option>
-									<option value='BAZAR'>BAZAR</option>
-									<option value='FERRETERÍA'>FERRETERÍA</option>
+									{categories.map(({ categoryName }) => (
+										<option value={categoryName}>{categoryName}</option>
+									))}
 								</Form.Control>
 							</Form.Group>
 							<Form.Group>
@@ -91,6 +123,44 @@ const CategoriesPage = () => {
 								Agregar
 							</Button>
 						</Form>
+					</div>
+				</Col>
+			</Row>
+
+			<Row>
+				<Col>
+					<div className='additional-interface-section'>
+						<header className='heard-tible-categoria'>
+							{' '}
+							Categoría y Subcategoría
+						</header>
+
+						<ul className='subcategory-list'>
+							{categories.map(({ categoryName, subCategories, id }) => (
+								<Col className='category-list-col'>
+									<span onClick={() => toggleCategory(`${categoryName}`)}>
+										<i className='bi bi-caret-down-fill'></i>
+										{categoryName}
+									</span>
+									<div className='category-buttons'>
+										<Button className='category-btn-edit'>Editar</Button>{' '}
+										<Button className='category-btn-delete'>Borrar</Button>{' '}
+										<input
+											type='checkbox'
+											id={id}
+											className='category-checkbox'
+										/>
+									</div>
+									{isCategoryExpanded(`${categoryName}`) && (
+										<ul className='subcategory-list'>
+											{subCategories.map(({ subCategoryName }) => (
+												<li>{subCategoryName}</li>
+											))}
+										</ul>
+									)}
+								</Col>
+							))}
+						</ul>
 					</div>
 				</Col>
 			</Row>
