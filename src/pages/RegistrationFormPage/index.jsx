e6f736ba77e4a302451e20f37bd2f56d814465fa
ngortiz <nidigortiz21@gmail.com>
2024-06-21 11:00:29 -0400

    RegistrationFormPage page refactoring is done

--- src/pages/RegistrationFormPage/index.jsx
+++ src/pages/RegistrationFormPage/index.jsx
@@ -1,7 +1,9 @@
+// RegistrationFormPage.js
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Form, Button, Spinner } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { useQuery, gql } from '@apollo/client';
+import RegistrationForm from '../../components/RegistrationForm';
 import './style.css';
 
 const GET_CATEGORIES_QUERY = gql`
@@ -34,7 +36,7 @@ const RegistrationFormPage = () => {
 	const [productDescription, setProductDescription] = useState('');
 	const [supplier, setSupplier] = useState('');
 	const [tax, setTax] = useState('');
-	const [template, setTemplates] = useState('');
+	const [template, setTemplate] = useState('');
 	const [category, setCategory] = useState('');
 	const [subcategory, setSubcategory] = useState('');
 	const [categories, setCategories] = useState([]);
@@ -44,7 +46,6 @@ const RegistrationFormPage = () => {
 
 	const { loading: loadingCategories, data: categoriesData } =
 		useQuery(GET_CATEGORIES_QUERY);
-
 	const { loading: loadingTemplates, data: templatesData } =
 		useQuery(GET_TEMPLATES_QUERY);
 
@@ -56,7 +57,7 @@ const RegistrationFormPage = () => {
 
 	useEffect(() => {
 		if (templatesData) {
-			setTemplates(templatesData.getTemplates);
+			setTemplate(templatesData.getTemplates);
 		}
 	}, [templatesData]);
 
@@ -97,197 +98,34 @@ const RegistrationFormPage = () => {
 						</h2>
 					</Col>
 				</Row>
-				<Form onSubmit={handleSubmit}>
-					<Row className='registration-form-row'>
-						<Col md={6}>
-							<Form.Group className='registration-form-group'>
-								<Form.Label className='registration-form-label'>
-									{t('registrationPage.code')}:*
-								</Form.Label>
-								<div className='d-flex'>
-									<Form.Control
-										type='text'
-										value={productCode}
-										readOnly
-										className='registration-form-input'
-									/>
-									<Button
-										variant='primary'
-										onClick={handleGenerateCode}
-										className='registration-form-button'
-									>
-										{t('registrationPage.generar')}
-									</Button>
-								</div>
-							</Form.Group>
-						</Col>
-					</Row>
-					<Row className='registration-form-row'>
-						<Col md={6}>
-							<Form.Group className='registration-form-group'>
-								<Form.Label className='registration-form-label'>
-									{t('registrationPage.name')}:*
-								</Form.Label>
-								<Form.Control
-									type='text'
-									value={productName}
-									onChange={e => setProductName(e.target.value)}
-									required
-									className='registration-form-input'
-								/>
-							</Form.Group>
-						</Col>
-					</Row>
-					<Row className='registration-form-row'>
-						<Col md={6}>
-							<Form.Group className='registration-form-group'>
-								<Form.Label className='registration-form-label'>
-									{t('registrationPage.description')}:*
-								</Form.Label>
-								<Form.Control
-									as='textarea'
-									rows={3}
-									value={productDescription}
-									onChange={e => setProductDescription(e.target.value)}
-									required
-									className='registration-form-input'
-								/>
-							</Form.Group>
-						</Col>
-					</Row>
-					<Row className='registration-form-row'>
-						<Col md={6}>
-							<Form.Group className='registration-form-group'>
-								<Form.Label className='registration-form-label'>
-									{t('registrationPage.supplier')}:*
-								</Form.Label>
-								<Form.Select
-									value={supplier}
-									onChange={e => setSupplier(e.target.value)}
-									required
-									className='registration-form-select'
-								>
-									<option value='' className='label-registration-select'>
-										{t('registrationPage.select')}
-									</option>
-									{suppliers.map((supplier, index) => (
-										<option key={index} value={supplier}>
-											{supplier}
-										</option>
-									))}
-								</Form.Select>
-							</Form.Group>
-						</Col>
-					</Row>
-					<Row className='registration-form-row'>
-						<Col md={6}>
-							<Form.Group className='registration-form-group'>
-								<Form.Label className='registration-form-label'>
-									{t('registrationPage.tax')}:*
-								</Form.Label>
-								<Form.Select
-									value={tax}
-									onChange={e => setTax(e.target.value)}
-									required
-									className='registration-form-select'
-								>
-									<option value='' className='label-registration-select'>
-										{t('registrationPage.select')}
-									</option>
-									{taxes.map((tax, index) => (
-										<option key={index} value={tax}>
-											{tax}
-										</option>
-									))}
-								</Form.Select>
-							</Form.Group>
-						</Col>
-					</Row>
-					<Row className='registration-form-row'>
-						<Col md={6}>
-							<Form.Group className='registration-form-group'>
-								<Form.Label className='registration-form-label'>
-									{t('registrationPage.template')}:*
-								</Form.Label>
-								{loadingTemplates ? (
-									<Spinner animation='border' />
-								) : (
-									<Form.Select
-										value={template}
-										onChange={e => setTemplate(e.target.value)}
-										required
-										className='registration-form-select'
-									>
-										<option value='' className='label-registration-select'>
-											{t('registrationPage.selectTemplate')}
-										</option>
-										{templatesData.getTemplates.map(template => (
-											<option key={template.id} value={template.id}>
-												{template.name}
-											</option>
-										))}
-									</Form.Select>
-								)}
-							</Form.Group>
-						</Col>
-					</Row>
-					<Row className='registration-form-row'>
-						<Col md={6}>
-							<Form.Group className='registration-form-group'>
-								<Form.Label className='registration-form-label'>
-									{t('registrationPage.category')}:*
-								</Form.Label>
-								{loadingCategories ? (
-									<Spinner animation='border' />
-								) : (
-									<Form.Select
-										value={category}
-										onChange={handleCategoryChange}
-										required
-										className='registration-form-select'
-									>
-										<option value='' className='label-registration-select'>
-											{t('registrationPage.selectCategory')}
-										</option>
-										{categories.map(category => (
-											<option key={category.id} value={category.id}>
-												{category.name}
-											</option>
-										))}
-									</Form.Select>
-								)}
-							</Form.Group>
-						</Col>
-					</Row>
-					<Row className='registration-form-row'>
-						<Col md={6}>
-							<Form.Group className='registration-form-group'>
-								<Form.Label className='registration-form-label'>
-									{t('registrationPage.subCategory')}:*
-								</Form.Label>
-								{loadingCategories ? (
-									<Spinner animation='border' />
-								) : (
-									<Form.Select
-										value={subcategory}
-										onChange={e => setSubcategory(e.target.value)}
-										required
-										className='registration-form-select'
-									>
-										<option value='' className='label-registration-select'>
-											{t('registrationPage.selectSubCategory')}
-										</option>
-										{subcategories.map(subcategory => (
-											<option key={subcategory.id} value={subcategory.id}>
-												{subcategory.name}
-											</option>
-										))}
-									</Form.Select>
-								)}
-							</Form.Group>
-						</Col>
-					</Row>
-				</Form>
+				<RegistrationForm
+					productCode={productCode}
+					setProductCode={setProductCode}
+					productName={productName}
+					setProductName={setProductName}
+					productDescription={productDescription}
+					setProductDescription={setProductDescription}
+					supplier={supplier}
+					setSupplier={setSupplier}
+					tax={tax}
+					setTax={setTax}
+					template={template}
+					setTemplate={setTemplate}
+					category={category}
+					setCategory={setCategory}
+					subcategory={subcategory}
+					setSubcategory={setSubcategory}
+					categories={categories}
+					subcategories={subcategories}
+					suppliers={suppliers}
+					taxes={taxes}
+					loadingTemplates={loadingTemplates}
+					loadingCategories={loadingCategories}
+					templatesData={templatesData}
+					handleGenerateCode={handleGenerateCode}
+					handleSubmit={handleSubmit}
+					handleCategoryChange={handleCategoryChange}
+				/>
 			</Container>
 		</>
 	);
