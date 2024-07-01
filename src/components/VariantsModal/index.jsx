29d135d472498d9aff8c6d95238fa0b7d8422d31
Nidia Ortiz <123881386+ngortiz@users.noreply.github.com>
2024-07-01 13:22:34 -0400

    Modal variants (#68)
    * add modal
    
    * modifications
    
    * add query
    
    * modifications

---
+++ src/components/VariantsModal/index.jsx
@@ -0,0 +1,147 @@
+import React from 'react';
+import { Modal, Table, Form, Container, Spinner, Alert } from 'react-bootstrap';
+import { useQuery, gql } from '@apollo/client';
+import { useTranslation } from 'react-i18next';
+import './style.css';
+
+const GET_PRODUCT_VARIANTS_QUERY = gql`
+	query GetProductVariants($limit: Int!, $offset: Int!, $productId: Int!) {
+		getProductVariants(limit: $limit, offset: $offset, productId: $productId) {
+			count
+			rows {
+				amount
+				code
+				costPrice
+				id
+				internalCode
+				name
+				offered
+				picture
+				published
+				productAttributes
+				sellPrice
+			}
+		}
+	}
+`;
+
+const VariantsModal = ({ show, handleClose, product }) => {
+	const { t } = useTranslation();
+	const { id: productId } = product;
+	const { loading, error, data } = useQuery(GET_PRODUCT_VARIANTS_QUERY, {
+		variables: { limit: 10, offset: 0, productId },
+	});
+
+	if (loading) {
+		return (
+			<Container>
+				<Spinner animation='border' />
+			</Container>
+		);
+	}
+
+	if (error) {
+		return (
+			<Container>
+				<Alert variant='danger'>Error: {error.message}</Alert>
+			</Container>
+		);
+	}
+
+	const variants = data?.getProductVariants?.rows || [];
+
+	const getVariantAttributes = attributes => {
+		return Object.entries(JSON.parse(attributes)).map(
+			([attributeName, attributeValue], index) => {
+				if (attributeValue.length > 0) {
+					return (
+						<div key={index}>
+							<strong>{attributeName}:</strong>
+							{attributeValue}
+							<br></br>
+						</div>
+					);
+				}
+			},
+		);
+	};
+
+	return (
+		<Container>
+			<Modal show={show} onHide={handleClose} size='xl'>
+				<Modal.Header closeButton>
+					<Modal.Title>{t('variantsModal.productVariants')}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<div className='modal-variants'>
+						<div className='searchContainer'>
+							<Form.Group
+								controlId='showEntries'
+								className='searchContainer-select'
+							>
+								<Form.Label className='modal-variantsShow-label'>
+									{t('variantsModal.show')}
+								</Form.Label>
+								<Form.Control as='select'>
+									<option>10</option>
+									<option>25</option>
+									<option>50</option>
+									<option>100</option>
+								</Form.Control>
+							</Form.Group>
+							<Form.Group controlId='search' className='searchModal-input'>
+								<Form.Label className='modal-variantsSearch-label'>
+									{t('variantsModal.search')}
+								</Form.Label>
+								<Form.Control
+									type='text'
+									placeholder={t('variantsModal.enterSearchTerm')}
+									className='searchContainer-input '
+								/>
+							</Form.Group>
+						</div>
+						<Table striped bordered hover className='variantsModal-table'>
+							<thead>
+								<tr>
+									<th> {t('variantsModal.image')}</th>
+									<th> {t('variantsModal.description')}</th>
+									<th> {t('variantsModal.barCode')}</th>
+									<th> {t('variantsModal.internalCode')}</th>
+									<th> {t('variantsModal.specification')}</th>
+									<th> {t('variantsModal.costPrice')}</th>
+									<th> {t('variantsModal.sellPrice')}</th>
+									<th> {t('variantsModal.amount')}</th>
+									<th> {t('variantsModal.published')}</th>
+									<th> {t('variantsModal.onOffer')}</th>
+								</tr>
+							</thead>
+							<tbody>
+								{variants.map(variant => (
+									<tr key={variant.id}>
+										<td>
+											<img
+												src={`${import.meta.env.VITE_IMAGE_URL}/images/%2Fstorage%2Fproducts%2Fpicture%2F${variant.id}%2Fthumb_${variant.picture}`}
+												alt={variant.name}
+											/>
+										</td>
+										<td>{variant.name}</td>
+										<td>{variant.code}</td>
+										<td>{variant.internalCode}</td>
+										<td>{getVariantAttributes(variant.productAttributes)}</td>
+										<td>US$ {variant.costPrice}</td>
+										<td>US$ {variant.sellPrice}</td>
+										<td>{variant.amount}</td>
+										<td>{variant.published ? 'Sí' : 'No'}</td>
+										<td>{variant.offered ? 'Sí' : 'No'}</td>
+									</tr>
+								))}
+							</tbody>
+						</Table>
+					</div>
+				</Modal.Body>
+			</Modal>
+		</Container>
+	);
+};
+
+export default VariantsModal;
