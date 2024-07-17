3b16331003fedc6d1308da67a0b88bc9a921a0ff
Nidia Ortiz <123881386+ngortiz@users.noreply.github.com>
2024-07-17 10:42:58 -0400

    Change Style (#82)
    * Change Style
    
    * modificacionts
    
    * new modications

--- src/components/Droppable/index.jsx
+++ src/components/Droppable/index.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import { useDroppable } from '@dnd-kit/core'
+import React from 'react';
+import { useDroppable } from '@dnd-kit/core';
 
 export default function Droppable(props) {
-  const { isOver, setNodeRef } = useDroppable({
-    id: props.id
-  })
+	const { isOver, setNodeRef } = useDroppable({
+		id: props.id,
+	});
 
-  const style = {
-    opacity: isOver ? 1 : 0.5,
-    minHeight: '50px',
-    margin: '10px'
-  }
+	const style = {
+		opacity: isOver ? 1 : 0.5,
+		minHeight: '50px',
+		margin: '10px',
+		backgroundColor: isOver ? '#333' : 'transparent',
+		transition: 'background-color 0.3s ease',
+	};
 
-  return (
-    <div ref={setNodeRef} style={style}>
-      {props.children}
-    </div>
-  )
+	return (
+		<div ref={setNodeRef} style={style}>
+			{props.children}
+		</div>
+	);
 }
