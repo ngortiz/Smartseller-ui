27e32ea29fc5c50adec06db3522c7f9cb114ba14
ngortiz <nidigortiz21@gmail.com>
2024-07-17 22:31:40 -0400

    New style

--- src/pages/CreateTemplatesPage/index.jsx
+++ src/pages/CreateTemplatesPage/index.jsx
@@ -61,97 +61,106 @@ const CreateTemplatesPage = () => {
 	};
 
 	return (
-		<Container className='create-templates-container'>
+		<>
 			<Row className='mt-4'>
 				<Col>
 					<header className='create-templates-header'>
 						{t('createTemplates.createTemplate')}
 					</header>
-					<Form>
-						<Form.Group controlId='formTemplateName'>
-							<Form.Label className='create-templates-form-label'>
-								{t('createTemplates.templateName')}
-							</Form.Label>
-							<Form.Control
-								type='text'
-								placeholder={t('createTemplates.enterTemplateName')}
-								value={templateName}
-								onChange={e => setTemplateName(e.target.value)}
-							/>
-						</Form.Group>
-						<Form.Group controlId='formAttributeName' className='mt-3'>
-							<Form.Label className='create-templates-form-label'>
-								{t('createTemplates.attributesName')}
-							</Form.Label>
-							<Form.Control
-								type='text'
-								placeholder={t('createTemplates.enterAttributesName')}
-								value={attributeName}
-								onChange={e => setAttributeName(e.target.value)}
-							/>
-						</Form.Group>
-						<div className='mt-3'>
-							<ul className='create-templates-attributes-list'>
-								{attributes.map((attr, index) => (
-									<li key={index}>{attr}</li>
-								))}
-							</ul>
-						</div>
-						<Button
-							className='create-templates-button'
-							onClick={handleCreateTemplate}
-						>
-							{t('createTemplates.add')}
-						</Button>
-					</Form>
 				</Col>
 			</Row>
-			<Row className='mt-5'>
-				<Col>
-					<Table striped bordered hover className='create-templates-table'>
-						<thead>
-							<tr>
-								<th>{t('createTemplates.template')}</th>
-								<th>{t('createTemplates.attributes')}</th>
-								<th>{t('createTemplates.delete')}</th>
-							</tr>
-						</thead>
-						<tbody>
-							{loading ? (
+			<Container className='create-templates-container'>
+				<Form>
+					<Form.Group controlId='formTemplateName'>
+						<Form.Label className='create-templates-form-label'>
+							{t('createTemplates.templateName')}
+						</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder={t('createTemplates.enterTemplateName')}
+							value={templateName}
+							onChange={e => setTemplateName(e.target.value)}
+							className='createTemplates-input'
+						/>
+					</Form.Group>
+					<Form.Group controlId='formAttributeName' className='mt-3'>
+						<Form.Label className='create-templates-form-label'>
+							{t('createTemplates.attributesName')}
+						</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder={t('createTemplates.enterAttributesName')}
+							value={attributeName}
+							onChange={e => setAttributeName(e.target.value)}
+							className='createTemplates-input'
+						/>
+					</Form.Group>
+					<div className='mt-3'>
+						<ul className='create-templates-attributes-list'>
+							{attributes.map((attr, index) => (
+								<li key={index}>{attr}</li>
+							))}
+						</ul>
+					</div>
+					<Button
+						className='create-templates-button'
+						onClick={handleCreateTemplate}
+					>
+						{t('createTemplates.add')}
+					</Button>
+				</Form>
+
+				<Row className='mt-5'>
+					<Col>
+						<Table striped bordered hover className='create-templates-table'>
+							<thead>
 								<tr>
-									<td colSpan='3' className='text-center'>
-										<Spinner animation='border' role='status'>
-											<span className='visually-hidden'>Loading...</span>
-										</Spinner>
-									</td>
+									<th>{t('createTemplates.template')}</th>
+									<th>{t('createTemplates.attributes')}</th>
+									<th>{t('createTemplates.delete')}</th>
 								</tr>
-							) : error ? (
-								<tr>
-									<td colSpan='3' className='text-center text-danger'>
-										{t('createTemplates.error')}: {error.message}
-									</td>
-								</tr>
-							) : (
-								templates.map((template, index) => (
-									<tr key={template.id || index}>
-										<td>{template.name}</td>
-										<td>{template.format}</td>
-										<td>
-											<Button
-												className='create-templates-delete-button'
-												onClick={() => handleDeleteTemplate(index)}
+							</thead>
+							<tbody>
+								{loading ? (
+									<tr>
+										<td colSpan='3' className='text-center'>
+											<Spinner
+												animation='border'
+												role='status'
+												variant='primary'
 											>
-												<i className='bi bi-trash3'></i>
-											</Button>
+												<span className='visually-hidden'>Loading...</span>
+											</Spinner>
 										</td>
 									</tr>
-								))
-							)}
-						</tbody>
-					</Table>
-				</Col>
-			</Row>
-		</Container>
+								) : error ? (
+									<tr>
+										<td colSpan='3' className='text-center text-danger'>
+											{t('createTemplates.error')}: {error.message}
+										</td>
+									</tr>
+								) : (
+									templates.map((template, index) => (
+										<tr key={template.id || index}>
+											<td>{template.name}</td>
+											<td>{template.format}</td>
+											<td>
+												<Button
+													className='create-templates-delete-button'
+													onClick={() => handleDeleteTemplate(index)}
+												>
+													<i className='bi bi-trash3'></i>
+												</Button>
+											</td>
+										</tr>
+									))
+								)}
+							</tbody>
+						</Table>
+					</Col>
+				</Row>
+			</Container>
+		</>
 	);
 };
 
