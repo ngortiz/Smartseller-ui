89ab8a86852657842fa9ae0d3f8913db50cb4976
Nidia Ortiz <123881386+ngortiz@users.noreply.github.com>
2024-07-19 14:28:53 -0400

    changes (#84)
    * changes
    
    * STYLE MODIFICATIONS

--- src/pages/RegisterProductsFromExcelPage/index.jsx
+++ src/pages/RegisterProductsFromExcelPage/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, Table } from 'react-bootstrap';
 import './style.css';
 
 const RegisterProductsFromExcelPage = () => {
@@ -26,135 +26,130 @@ const RegisterProductsFromExcelPage = () => {
 			<header className='unique-title-header'>
 				{t('registerProductsFromExcel.massiveIncome')}
 			</header>
-			<header className='unique-page-header'>
-				{t('registerProductsFromExcel.enterFormToLoadProducts')}
-			</header>
-			<Form className='unique-form'>
-				<Form.Group controlId='formExcelFile' className='mb-3'>
-					<Form.Label>
-						{t('registerProductsFromExcel.selectExcelFile')}:
-					</Form.Label>
-					<Form.Control type='file' accept='.xlsx, .xls' />
-				</Form.Group>
-				<Form.Group controlId='formZipFile' className='mb-3'>
-					<Form.Label>
-						{t('registerProductsFromExcel.selectZIPFile')}:
-					</Form.Label>
-					<Form.Control type='file' accept='.zip' />
-				</Form.Group>
-			</Form>
-			<Button
-				variant='primary'
-				onClick={handleUpload}
-				className='unique-button'
-			>
-				{t('registerProductsFromExcel.charge')}
-			</Button>
-			<Row>
-				<Col>
-					<header className='unique-subHeard'>
-						{t('registerProductsFromExcel.requiredColumns')}
-					</header>
-					<Card className='unique-card'>
-						<Card.Body>
-							<Card.Title className='unique-card-title'>
-								{t('registerProductsFromExcel.name')}
-							</Card.Title>
-							<ul className='unique-list'>
+			<Container className='registerProduct-container'>
+				<header className='unique-page-header'>
+					{t('registerProductsFromExcel.enterFormToLoadProducts')}
+				</header>
+				<Form className='unique-form'>
+					<Form.Group controlId='formExcelFile' className='mb-3'>
+						<Form.Label className='label-register'>
+							{t('registerProductsFromExcel.selectExcelFile')}:
+						</Form.Label>
+						<Form.Control type='file' accept='.xlsx, .xls' />
+					</Form.Group>
+					<Form.Group controlId='formZipFile' className='mb-3'>
+						<Form.Label className='label-register'>
+							{t('registerProductsFromExcel.selectZIPFile')}:
+						</Form.Label>
+						<Form.Control type='file' accept='.zip' />
+					</Form.Group>
+				</Form>
+				<Button
+					variant='primary'
+					onClick={handleUpload}
+					className='unique-button'
+				>
+					{t('registerProductsFromExcel.charge')}
+				</Button>
+			</Container>
+			<Container className='registerProduct-container'>
+				<Row className='mt-4'>
+					<Col>
+						<Table bordered hover className='unique-table'>
+							<thead>
+								<tr>
+									<th>{t('registerProductsFromExcel.requiredColumns')}</th>
+								</tr>
+							</thead>
+							<tbody>
 								{COLUMN_NAMES.map((name, index) => (
-									<li key={index} className='unique-list-item'>
-										{name}
-									</li>
+									<tr key={index}>
+										<td>{name}</td>
+									</tr>
 								))}
-							</ul>
-						</Card.Body>
-					</Card>
-				</Col>
-				<Col>
-					<header className='unique-subHeard'>
-						{t('registerProductsFromExcel.availableTaxType')}
-					</header>
-					<Card className='unique-card'>
-						<Card.Body>
-							<Card.Title className='unique-card-title'>
-								{t('registerProductsFromExcel.name')}
-							</Card.Title>
-							<ul className='unique-list'>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.iva10')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.iva5')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.exempt')}
-								</li>
-							</ul>
-						</Card.Body>
-					</Card>
-				</Col>
-				<Col>
-					<header className='unique-subHeard'>
-						{t('registerProductsFromExcel.availableCategories')}
-					</header>
-					<Card className='unique-card'>
-						<Card.Body>
-							<Card.Title className='unique-card-title'>
-								{t('registerProductsFromExcel.name')}
-							</Card.Title>
-							<ul className='unique-list'>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.bazar')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.hardwareStore')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.home')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.illuminaries')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.toyStore')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.bookshop')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.christmasItems')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.beachesCampingOutdoors')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.automobiles')}
-								</li>
-
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.cotillon')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.appliance')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.closet')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.cosmetic')}
-								</li>
-
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.cleaningProduct')}
-								</li>
-								<li className='unique-list-item'>
-									{t('registerProductsFromExcel.accessories')}
-								</li>
-							</ul>
-						</Card.Body>
-					</Card>
-				</Col>
-			</Row>
+							</tbody>
+						</Table>
+					</Col>
+					<Col>
+						<Table bordered hover className='unique-table'>
+							<thead>
+								<tr>
+									<th>{t('registerProductsFromExcel.availableTaxType')}</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>{t('registerProductsFromExcel.iva10')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.iva5')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.exempt')}</td>
+								</tr>
+							</tbody>
+						</Table>
+					</Col>
+					<Col>
+						<Table bordered hover className='unique-table'>
+							<thead>
+								<tr>
+									<th>{t('registerProductsFromExcel.availableCategories')}</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>{t('registerProductsFromExcel.bazar')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.hardwareStore')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.home')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.illuminaries')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.toyStore')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.bookshop')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.christmasItems')}</td>
+								</tr>
+								<tr>
+									<td>
+										{t('registerProductsFromExcel.beachesCampingOutdoors')}
+									</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.automobiles')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.cotillon')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.appliance')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.closet')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.cosmetic')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.cleaningProduct')}</td>
+								</tr>
+								<tr>
+									<td>{t('registerProductsFromExcel.accessories')}</td>
+								</tr>
+							</tbody>
+						</Table>
+					</Col>
+				</Row>
+			</Container>
 		</div>
 	);
 };
