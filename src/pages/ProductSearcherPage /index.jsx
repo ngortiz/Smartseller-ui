80a0e1c28093cfd33e3ade5613e6906204d04e7d
Nidia Ortiz <123881386+ngortiz@users.noreply.github.com>
2024-07-24 13:42:00 -0400

    Style modification is made (#85)
    * STYLE MODIFICATIONS
    
    * changes
    
    * New STYLE MODIFICATIONS
    
    * Style Modification

--- src/pages/ProductSearcherPage /index.jsx
+++ src/pages/ProductSearcherPage /index.jsx
@@ -148,184 +148,206 @@ const ProductSearcherPage = () => {
 	};
 
 	return (
-		<Container id='product-searcher-page'>
+		<>
 			<Row className='mt-4'>
-				<header className='product-header'>
+				<header className='product-searcher-heard'>
 					{t('productSearcherPage.searchVariant')}
 				</header>
 			</Row>
-			<Row className='mt-4'>
-				<Form.Group
-					as={Col}
-					controlId='formInternalCode'
-					className='product-form-control'
-				>
-					<Form.Label>{t('productSearcherPage.internoCode')}:</Form.Label>
-					<Form.Control
-						type='text'
-						placeholder={t('productSearcherPage.enterTheInternalCode')}
-						value={internalCode}
-						onChange={e => setInternalCode(e.target.value)}
-						className='product-form-control'
-					/>
-				</Form.Group>
-				<Form.Group
-					as={Col}
-					controlId='formBarcode'
-					className='product-form-control'
-				>
-					<Form.Label>{t('productSearcherPage.baCode')}:</Form.Label>
-					<Form.Control
-						type='text'
-						placeholder={t('productSearcherPage.enterBarCode')}
-						value={barcode}
-						onChange={e => setBarcode(e.target.value)}
-						className='product-form-control'
-					/>
-				</Form.Group>
-
-				<Form.Group as={Col} controlId='formDescription'>
-					<Form.Label>{t('productSearcherPage.description')}:</Form.Label>
-					<Form.Control
-						type='text'
-						placeholder={t('productSearcherPage.enterDescription')}
-						value={description}
-						onChange={e => setDescription(e.target.value)}
-						className='product-form-control'
-					/>
-				</Form.Group>
-
-				<Form.Group as={Col} controlId='formCategory'>
-					<Form.Label>{t('productSearcherPage.categories')}:</Form.Label>
-					<Form.Select
-						aria-label={t('productSearcherPage.category')}
-						value={category}
-						onChange={e => setCategory(e.target.value)}
-						className='product-form-select'
+			<Container id='product-searcher-page'>
+				<Row className='mt-4'>
+					<Form.Group
+						as={Col}
+						controlId='formInternalCode'
+						className='productS-form-control'
 					>
-						{categoriesLoading ? (
-							<option>{t('productSearcherPage.loading')}</option>
-						) : (
-							<>
-								<option value=''>{t('productSearcherPage.select')}...</option>
-								{categoriesData &&
-									categoriesData.getCategories.map(category => (
-										<option key={category.id} value={category.id}>
-											{category.name}
-										</option>
-									))}
-							</>
-						)}
-					</Form.Select>
-				</Form.Group>
-				<Form.Group
-					as={Col}
-					controlId='formCheckbox'
-					className='product-form-input'
-				>
-					<Form.Check
-						type='checkbox'
-						label={t('productSearcherPage.post')}
-						checked={checked}
-						onChange={e => setChecked(e.target.checked)}
-						className='product-check-input'
-					/>
-				</Form.Group>
-
-				<Col className='mt-3'>
-					<Button
-						variant='primary'
-						onClick={() => handleSearch(1, limit)}
-						className='product-button'
+						<Form.Label className='product-searcher-label'>
+							{t('productSearcherPage.internoCode')}:
+						</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder={t('productSearcherPage.enterTheInternalCode')}
+							value={internalCode}
+							onChange={e => setInternalCode(e.target.value)}
+							className='productS-form-control'
+						/>
+					</Form.Group>
+					<Form.Group
+						as={Col}
+						controlId='formBarcode'
+						className='productS-form-control'
 					>
-						<i className='bi bi-search'></i> {t('productSearcherPage.searcher')}
-					</Button>
-				</Col>
-			</Row>
+						<Form.Label className='product-searcher-label'>
+							{t('productSearcherPage.baCode')}:
+						</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder={t('productSearcherPage.enterBarCode')}
+							value={barcode}
+							onChange={e => setBarcode(e.target.value)}
+							className='productS-form-control'
+						/>
+					</Form.Group>
 
-			<Row className='mt-4'>
-				<Table striped bordered hover className='product-table'>
-					<thead>
-						<tr>
-							<th>{t('productSearcherPage.productCode')}</th>
-							<th>{t('productSearcherPage.internalCode')}</th>
-							<th>{t('productSearcherPage.barCode')}</th>
-							<th>{t('productSearcherPage.mainProduct')}</th>
-							<th>{t('productSearcherPage.description')}</th>
-							<th>{t('productSearcherPage.stock')}</th>
-							<th>{t('productSearcherPage.category')}</th>
-							<th>{t('productSearcherPage.subCategory')}</th>
-							<th>{t('productSearcherPage.costPrice')}</th>
-							<th>{t('productSearcherPage.salePrice')}</th>
-							<th>{t('productSearcherPage.inOffer')}</th>
-							<th>{t('productSearcherPage.specification')}</th>
-							<th>{t('productSearcherPage.edit')}</th>
-							<th>{t('productSearcherPage.delete')}</th>
-						</tr>
-					</thead>
-					<tbody>
-						{searchLoading ? (
+					<Form.Group
+						as={Col}
+						controlId='formDescription'
+						className='productS-form-control'
+					>
+						<Form.Label className='product-searcher-label'>
+							{t('productSearcherPage.description')}:
+						</Form.Label>
+						<Form.Control
+							type='text'
+							placeholder={t('productSearcherPage.enterDescription')}
+							value={description}
+							onChange={e => setDescription(e.target.value)}
+							className='productS-form-control'
+						/>
+					</Form.Group>
+
+					<Form.Group
+						as={Col}
+						controlId='formCategory'
+						className='productS-form-select'
+					>
+						<Form.Label>{t('productSearcherPage.categories')}:</Form.Label>
+						<Form.Select
+							aria-label={t('productSearcherPage.category')}
+							value={category}
+							onChange={e => setCategory(e.target.value)}
+							className='productS-form-select'
+						>
+							{categoriesLoading ? (
+								<option>{t('productSearcherPage.loading')}</option>
+							) : (
+								<>
+									<option value=''>{t('productSearcherPage.select')}...</option>
+									{categoriesData &&
+										categoriesData.getCategories.map(category => (
+											<option key={category.id} value={category.id}>
+												{category.name}
+											</option>
+										))}
+								</>
+							)}
+						</Form.Select>
+					</Form.Group>
+					<Form.Group
+						as={Col}
+						controlId='formCheckbox'
+						className='productS-form-input'
+					>
+						<Form.Label className='productS-check-label'>
+							{t('productSearcherPage.post')}:
+						</Form.Label>
+						<Form.Check
+							type='checkbox'
+							label=''
+							checked={checked}
+							onChange={e => setChecked(e.target.checked)}
+							className='productS-check-input'
+						/>
+					</Form.Group>
+
+					<Col className='mt-3'>
+						<Button
+							variant='primary'
+							onClick={() => handleSearch(1, limit)}
+							className='productS-button'
+						>
+							<i className='bi bi-search'></i>{' '}
+							{t('productSearcherPage.searcher')}
+						</Button>
+					</Col>
+				</Row>
+			</Container>
+			<Container id='product-searcher-page'>
+				<Row className='mt-4'>
+					<Table bordered hover className='productS-table'>
+						<thead>
 							<tr>
-								<td colSpan='14' className='text-center'>
-									<Spinner animation='border' role='status'>
-										<span className='visually-hidden'>Loading...</span>
-									</Spinner>
-								</td>
+								<th>{t('productSearcherPage.productCode')}</th>
+								<th>{t('productSearcherPage.internalCode')}</th>
+								<th>{t('productSearcherPage.barCode')}</th>
+								<th>{t('productSearcherPage.mainProduct')}</th>
+								<th>{t('productSearcherPage.description')}</th>
+								<th>{t('productSearcherPage.stock')}</th>
+								<th>{t('productSearcherPage.category')}</th>
+								<th>{t('productSearcherPage.subCategory')}</th>
+								<th>{t('productSearcherPage.costPrice')}</th>
+								<th>{t('productSearcherPage.salePrice')}</th>
+								<th>{t('productSearcherPage.inOffer')}</th>
+								<th>{t('productSearcherPage.specification')}</th>
+								<th>{t('productSearcherPage.edit')}</th>
+								<th>{t('productSearcherPage.delete')}</th>
 							</tr>
-						) : (
-							products.map(variant => (
-								<tr key={variant.id}>
-									<td>{variant.product.code}</td>
-									<td>{variant.internalCode}</td>
-									<td>{variant.code}</td>
-									<td>{variant.product.name}</td>
-									<td>{variant.name}</td>
-									<td>{variant.amount}</td>
-									<td>{variant.product?.category?.name || ''}</td>
-									<td>{variant.product?.subCategory?.name || ''}</td>
-									<td>US$ {variant.costPrice}</td>
-									<td>US$ {variant.sellPrice}</td>
-									<td>{variant.offered ? 'Sí' : 'No'}</td>
-									<td>{getVariantAttributes(variant.productAttributes)}</td>
-									<td>
-										<Button
-											variant='info'
-											onClick={() => handleEditProduct(variant.id)}
-											className='product-button-edit'
-										>
-											<i className='bi bi-pencil-square'></i>
-										</Button>
-									</td>
-									<td>
-										<Button
-											className='product-button-delete'
-											variant='danger'
-											onClick={() => handleDeleteProduct(variant.id)}
-										>
-											<i className='bi bi-trash3'></i>
-										</Button>
+						</thead>
+						<tbody>
+							{searchLoading ? (
+								<tr>
+									<td colSpan='14' className='text-center'>
+										<Spinner animation='border' role='status'>
+											<span className='visually-hidden'>Loading...</span>
+										</Spinner>
 									</td>
 								</tr>
-							))
-						)}
-					</tbody>
-				</Table>
-			</Row>
-			<Row>
-				<Col className='d-flex justify-content-center'>
-					<PaginationControl
-						page={page}
-						total={total}
-						limit={limit}
-						changePage={page => {
-							setPage(page);
-							handleSearch(page, limit);
-						}}
-						ellipsis={1}
-					/>
-				</Col>
-			</Row>
-		</Container>
+							) : (
+								products.map(variant => (
+									<tr key={variant.id}>
+										<td>{variant.product.code}</td>
+										<td>{variant.internalCode}</td>
+										<td>{variant.code}</td>
+										<td>{variant.product.name}</td>
+										<td>{variant.name}</td>
+										<td>{variant.amount}</td>
+										<td>{variant.product?.category?.name || ''}</td>
+										<td>{variant.product?.subCategory?.name || ''}</td>
+										<td>US$ {parseFloat(variant.costPrice).toFixed(2)}</td>
+										<td>US$ {parseFloat(variant.sellPrice).toFixed(2)}</td>
+										<td>{variant.offered ? 'Sí' : 'No'}</td>
+										<td>{getVariantAttributes(variant.productAttributes)}</td>
+										<td>
+											<Button
+												variant='info'
+												onClick={() => handleEditProduct(variant.id)}
+												className='productS-button-edit'
+											>
+												<i className='bi bi-pencil-square'></i>
+											</Button>
+										</td>
+										<td>
+											<Button
+												className='productS-button-delete'
+												variant='danger'
+												onClick={() => handleDeleteProduct(variant.id)}
+											>
+												<i className='bi bi-trash3'></i>
+											</Button>
+										</td>
+									</tr>
+								))
+							)}
+						</tbody>
+					</Table>
+				</Row>
+
+				<Row>
+					<Col className='d-flex justify-content-center'>
+						<PaginationControl
+							page={page}
+							total={total}
+							limit={limit}
+							changePage={page => {
+								setPage(page);
+								handleSearch(page, limit);
+							}}
+							ellipsis={1}
+						/>
+					</Col>
+				</Row>
+			</Container>
+		</>
 	);
 };
 
